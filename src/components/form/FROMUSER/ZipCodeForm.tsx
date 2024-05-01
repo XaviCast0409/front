bf5d4cf8a7dcecff06f1ea3f9bef3e06829b1116
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2024-05-01 19:45:00 -0300

    Actualizar estilos y funcionalidad en ZipCodeForm.tsx

--- src/components/form/FROMUSER/ZipCodeForm.tsx
+++ src/components/form/FROMUSER/ZipCodeForm.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import BackButtonArrow from "../../../utils/BackButtonArrow";
 import { useZipcCodeHook } from "../../../hooks/hookZipCode/useZipCodeHook";
 import { useUserHook } from "../../../hooks/hookUser/useUserHook";
+import { FormContainer } from "../../../utils/FormContainer";
 
 export default function ZipCodeForm() {
   const {
@@ -13,24 +14,27 @@ export default function ZipCodeForm() {
     message,
     validate,
     handleChange,
-    setMessage
+    setMessage,
   } = useZipcCodeHook();
   const { filterRegisterUser } = useUserHook();
   const navigate = useNavigate();
 
   const createZipCode = () => {
     onSubmitCreate(zipCode ?? {});
-    setMessage()
-    filterRegisterUser({ zipcode: Number(zipCodeNumber?.zipCode), tradeId: 0, classId: 0 })
+    setMessage();
+    filterRegisterUser({
+      zipcode: Number(zipCodeNumber?.zipCode),
+      tradeId: 0,
+      classId: 0,
+    });
     setTimeout(() => {
-      navigate("/form/tradeUser")
-    }, 500)
+      navigate("/form/tradeUser");
+    }, 500);
   };
-  
+
   return (
-    <div className="flex flex-col min-h-screen">
-      <section className="container_form_column" style={{ width: "60%" }}>
-        <h1>Validate zipCode</h1>
+    <div className="flex flex-col min-h-screen justify-center items-center">
+      <FormContainer title=" Validate Zip Code ">
         <InputForm
           id="floating_postal_code"
           nameInput="zipCode"
@@ -40,30 +44,30 @@ export default function ZipCodeForm() {
           handleChange={handleChange}
           color="black"
           title="Postal Code"
-          className="w-1/2"
+          className="w-80 md:w-full lg:w-96"
         />
-        {validate === false ? (
-          <div>
-            <p className="text-center text-red-600">{`${message}`}</p>
-          </div>
-        ) : (
-          <div>
-            <p className="text-center text-green-600">
-              {zipCode?.city} {zipCode?.state}
-            </p>
-          </div>
-        )}
-        <div className="container_buttons gap-8">
-          <BackButtonArrow />
-            <Button
-              className="btn-primary"
-              type="button"
-              text="SUBMIT"
-              handleClick={createZipCode}
-              disabled={!validate}
-            />
+      </FormContainer>
+      {validate === false ? (
+        <div>
+          <p className="text-center text-red-600">{`${message}`}</p>
         </div>
-      </section>
+      ) : (
+        <div>
+          <p className="text-center text-green-600">
+            {zipCode?.city} {zipCode?.state}
+          </p>
+        </div>
+      )}
+      <div className="container_buttons gap-8">
+        <BackButtonArrow />
+        <Button
+          className="btn-primary"
+          type="button"
+          text="SUBMIT"
+          handleClick={createZipCode}
+          disabled={!validate}
+        />
+      </div>
     </div>
   );
 }
