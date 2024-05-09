5e0f4b632f7c06b9cfd3566fd0142ff2e7175788
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2024-05-09 17:46:46 -0300

    feat: Update Next Steps section in MatchingNextSteps component

--- src/components/form/MatchingForm/MatchingFeatured.tsx
+++ src/components/form/MatchingForm/MatchingFeatured.tsx
@@ -1,125 +1,27 @@
-import { useState } from "react";
+import logo1 from "../../../../src/assets/asset/images/bathroom-icons.svg";
+
 
 export default function MatchingFeatured() {
-  const [reportsOpen1, setReportsOpen1] = useState(false);
-  const [reportsOpen2, setReportsOpen2] = useState(false);
-  const [reportsOpen3, setReportsOpen3] = useState(false);
-
   return (
-    <div className="mx-auto px-4 sm:px-6 lg:px-8 m-2">
-      <div className="flex flex-col p-2 lg:flex-row lg:space-x-4">
-        {/* Primer acordeón */}
-        <div className="w-full lg:w-1/3 mb-8 lg:mb-0">
-          <div
-            onClick={() => setReportsOpen1(!reportsOpen1)}
-            className="flex items-center text-gray-600 border-b cursor-pointer"
-          >
-            <div
-              className={`w-10 border-r px-2 transform ${reportsOpen1 ? "rotate-90" : ""}`}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-6 h-6 transition-transform duration-300 ease-in-out"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-                />
-              </svg>
-            </div>
-            <div className="px-2 py-3">
-              <button className="hover:underline">
-                This is where you click to open
-              </button>
-            </div>
-          </div>
-          <div className={`p-5 ${reportsOpen1 ? "" : "hidden"}`}>
-            This is a very simple dropdown/accordion/collapse (whatever you call
-            it) using Tailwind, Alpine.js, and the Alpine.js plugin "Collapse"
-            to enable smoother open/collapse transitions than what comes out of
-            the box with Alpine.js
-          </div>
+    <section className="text-gray-600 body-font">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-col text-center w-full mb-20">
+        <h2 className="text-5xl font-bold text-center dark:text-gray-900">
+          Your Matching <b className="primary_color">Pros</b>
+        </h2>
         </div>
-        {/* Segundo acordeón */}
-        <div className="w-full lg:w-1/3 mb-8 lg:mb-0">
-          <div
-            onClick={() => setReportsOpen2(!reportsOpen2)}
-            className="flex items-center text-gray-600 border-b cursor-pointer"
-          >
-            <div
-              className={`w-10 border-r px-2 transform ${reportsOpen2 ? "rotate-90" : ""}`}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-6 h-6 transition-transform duration-300 ease-in-out"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-                />
-              </svg>
-            </div>
-            <div className="px-2 py-3">
-              <button className="hover:underline">
-                This is where you click to open
-              </button>
-            </div>
+        <div className="flex flex-col justify-between overflow-hidden text-left transition-shadow duration-200 bg-white rounded shadow-xl group hover:shadow-2xl h-1/2">
+          <div className="p-5 flex flex-col justify-center items-center">
+            <img className="w-1/6  m-8" src={logo1} alt="img constructor" />
+            <h3 className="primary_color font-semibold">Bathroom</h3>
+            <p className="text-sm leading-5 text-gray-900">
+              Straightforward Bathroom Renovations to suit your needs. We can do
+              it all.
+            </p>
           </div>
-          <div className={`p-5 ${reportsOpen2 ? "" : "hidden"}`}>
-            This is a very simple dropdown/accordion/collapse (whatever you call
-            it) using Tailwind, Alpine.js, and the Alpine.js plugin "Collapse"
-            to enable smoother open/collapse transitions than what comes out of
-            the box with Alpine.js
-          </div>
-        </div>
-        {/* Tercer acordeón */}
-        <div className="w-full lg:w-1/3 mb-8 lg:mb-0">
-          <div
-            onClick={() => setReportsOpen3(!reportsOpen3)}
-            className="flex items-center text-gray-600 border-b cursor-pointer"
-          >
-            <div
-              className={`w-10 border-r px-2 transform ${reportsOpen3 ? "rotate-90" : ""}`}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-6 h-6 transition-transform duration-300 ease-in-out"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-                />
-              </svg>
-            </div>
-            <div className="px-2 py-3">
-              <button className="hover:underline">
-                This is where you click to open
-              </button>
-            </div>
-          </div>
-          <div className={`p-5 ${reportsOpen3 ? "" : "hidden"}`}>
-            This is a very simple dropdown/accordion/collapse (whatever you call
-            it) using Tailwind, Alpine.js, and the Alpine.js plugin "Collapse"
-            to enable smoother open/collapse transitions than what comes out of
-            the box with Alpine.js
-          </div>
+          <div className="w-full h-1 ml-auto duration-300 origin-left transform scale-x-0 bg-deep-purple-accent-400 group-hover:scale-x-100" />
         </div>
       </div>
-    </div>
+    </section>
   );
 }
--- src/components/form/MatchingForm/MatchingProsForm.tsx
+++ src/components/form/MatchingForm/MatchingProsForm.tsx
@@ -1,4 +1,4 @@
-//import MatchingFeatured from "./MatchingFeatured";
+import MatchingFeatured from "./MatchingFeatured";
 import MatchingDetail from "./MatchingDetail";
 import MatchingNextSteps from "./MatchingNextSteps";
 import { useNavigate } from "react-router-dom";
@@ -6,7 +6,6 @@ import { Link } from "react-router-dom";
 import Button from "../../../utils/Button";
 
 export default function MatchingProsForm() {
-
   const navigate = useNavigate();
   const handleSubmit = () => {
     navigate("/home");
@@ -14,17 +13,15 @@ export default function MatchingProsForm() {
 
   return (
     <>
-      <div className="w-full">
-        {" "}
-        {/* Agregado: clase w-full */}
-        <h1 className="h1">HERE ARE YOUR MATCHING PROS</h1>
-      </div>
+      <div className="w-full"> {/* Agregado: clase w-full */}</div>
+
       <div className="flex flex-col justify-center items-center lg:justify-center">
-        {/* Primer componente 
+        <h1 className="h1 mt-20">HERE ARE YOUR MATCHING PROS</h1>
+        {/* Primer componente */}
         <div className="mb-8 lg:mr-4">
           <MatchingFeatured />
         </div>
-         Segundo componente */}
+
         <div className="mb-8 mx-auto lg:mx-0  p-4">
           {" "}
           {/* Ajustes para MatchingDetail */}
@@ -35,18 +32,17 @@ export default function MatchingProsForm() {
           {/* Ajustes para MatchingNextSteps */}
           <MatchingNextSteps />
         </div>
-       
 
-                <div className="flex justify-center items-center my-10">
-                  <Link to="/">
-                    <Button
-                      className="btn-primary m-"
-                      type="button"
-                      text="Back To Home"
-                      handleClick={handleSubmit}
-                    />
-                  </Link>
-                </div>
+        <div className="flex justify-center items-center my-10">
+          <Link to="/">
+            <Button
+              className="btn-primary m-"
+              type="button"
+              text="Back To Home"
+              handleClick={handleSubmit}
+            />
+          </Link>
+        </div>
       </div>
     </>
   );
