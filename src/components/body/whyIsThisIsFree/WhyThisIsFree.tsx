f430d8983379c69fd7390dda1c8fbe9136b4ab90
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2024-05-09 21:14:37 -0300

    Actualizar diseño y estilos en el componente WhatToExpect

--- src/components/body/whyIsThisIsFree/WhyThisIsFree.tsx
+++ src/components/body/whyIsThisIsFree/WhyThisIsFree.tsx
@@ -18,9 +18,9 @@ export default function WhyThisIsFree() {
           </div>
           <div className="max-w-xl px-4 space-y-3 mt-6 sm:px-0 md:mt-0 lg:max-w-2xl">
             
-            <p className="text-gray-800 text-3xl font-semibold sm:text-4xl">
-              Why is this FREE?
-            </p>
+          <h2 className="text-5xl font-bold text-center dark:text-gray-900">
+              Why is this FREE ?
+            </h2>
             <p className="mt-3 text-gray-600">
               We believe finding project costs and booking estimates with
               contractors should  be a <br />simple process.
