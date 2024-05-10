44e72df95b40b45231b83cd1a601dbfaf4deb3d0
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2024-05-10 14:41:01 -0300

    feat: Update StreetAddressForm validation schema and error handling

--- src/components/form/FROMUSER/StreetAddressForm.tsx
+++ src/components/form/FROMUSER/StreetAddressForm.tsx
@@ -6,23 +6,41 @@ import CheckBox from "../../../utils/CheckBox";
 import { useState } from "react";
 import { useUserHook } from "../../../hooks/hookUser/useUserHook";
 import { FormContainer } from "../../../utils/FormContainer";
+import * as Yup from "yup";
 
 export default function StreetAddressForm() {
-  const { handleChange, userData, setUserStore, useStoreCreate } = useUserHook();
+  const { handleChange, userData, setUserStore, } = useUserHook();
   const navigate = useNavigate();
   const [isChecked, setIsChecked] = useState(false);
+  const [error, setError] = useState("");
 
-  
-  const handleSubmit = () => {
-    setUserStore({
-      address: userData.address
-    });
-    navigate("/emailphone");
+  const validationSchema = Yup.object().shape({
+    address: Yup.string()
+      .required("The street address is required")
+      .min(3, "The address must be at least 3 characters long")
+      .matches(/\d/, "The address must contain at least one number"),
+  });
+
+  const handleValidate = async () => {
+    try {
+      await validationSchema.validate({ address: userData.address });
+      setUserStore({ address: userData.address });
+      setError("");
+      return true;
+    } catch (error: any) {
+      setError(error.message);
+      return false;
+    }
   };
-  console.log(useStoreCreate)
+
+  const handleSubmit = async () => {
+    const isValid = await handleValidate();
+    if (isValid) navigate("/emailphone");
+  };
+
   return (
     <div className="flex flex-col min-h-screen justify-center items-center">
-     <FormContainer title=" Enter your Street Address ">
+      <FormContainer title=" Enter your Street Address ">
         <InputForm
           id="floating_address"
           nameInput="address"
@@ -42,19 +60,19 @@ export default function StreetAddressForm() {
             handleChange={(event) => setIsChecked(event.target.checked)}
           />
         </div>
-        </FormContainer>
-        <div className="container_buttons flex ">
-          <BackButtonArrow />
-          {Button && (
-            <Button
-              className="btn-primary"
-              type={"button"}
-              text="NEXT"
-              handleClick={handleSubmit}
-            />
-          )}
-        </div>
-     
+      </FormContainer>
+      {error && <p className="text-red-500">{error}</p>}
+      <div className="container_buttons flex ">
+        <BackButtonArrow />
+        {Button && (
+          <Button
+            className="btn-primary"
+            type={"button"}
+            text="NEXT"
+            handleClick={handleSubmit}
+          />
+        )}
+      </div>
     </div>
   );
 }
