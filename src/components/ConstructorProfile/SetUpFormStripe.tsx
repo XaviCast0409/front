8ff5eb657b8fc1afd2ee21305903114ebf320f50
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2024-05-17 13:43:45 -0300

    feat: Update API endpoint for fetching company data in SetUpFormStripe component

--- src/components/ConstructorProfile/SetUpFormStripe.tsx
+++ src/components/ConstructorProfile/SetUpFormStripe.tsx
@@ -28,7 +28,7 @@ const CheckoutForm = ({ handlePayment }) => {
   useEffect(() => {
     const fetchCompany = async () => {
       try {
-        const response = await axios.get(`/company-by-id/${id}`);
+        const response = await axios.get(`http://localhost:3000/company-by-id/${id}`);
         findCompanyById(response.data.id);
         console.log("response", response.data.id)
       } catch (error) {
