ba1e0913217e1ab4cf559a108554d974d53fa6c3
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2024-05-18 17:20:41 -0300

    feat: Remove social media icons from ImagenSigIn component

--- src/components/ConstructorProfile/ImagenSigIn.tsx
+++ src/components/ConstructorProfile/ImagenSigIn.tsx
@@ -1,8 +1,3 @@
-import InstagramWhiteLogo from "../../assets/asset/images/hows-advisor-instagram-white.png";
-import FacebookWhiteLogo from "../../assets/asset/images/hows-advisor-facebook-white.png";
-import LinkedinWhiteLogo from "../../assets/asset/images/hows-advisor-linkedin-white.png";
-import WhatsAppLogo from "../../assets/asset/images/hows-advisor-whatsapp-white.png";
-
 export const ImagenSigIn = () => {
   return (
     <div
@@ -20,34 +15,6 @@ export const ImagenSigIn = () => {
         <p className="text-3xl my-4 text-white">
           Crafting your spaces with precision and care, every step of the way.
         </p>
-        <div className=" mx-4 p-4 text-center flex justify-center  items-center">
-          <a
-            href="https://www.instagram.com/howsadvisor/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src={InstagramWhiteLogo}
-              className="mx-4 w-10 h-10 my-2"
-              alt="Img Instagram"
-            />
-          </a>
-          <img
-            src={FacebookWhiteLogo}
-            className="mx-4 w-10 h-10 my-2 "
-            alt="Img Facebook"
-          />
-          <img
-            src={LinkedinWhiteLogo}
-            className="mx-4 my-2 w-10 h-10"
-            alt="Img Linkedin"
-          />
-          <img
-            src={WhatsAppLogo}
-            className="mx-4 my-2 w-10 h-10"
-            alt="Img Whatsapp"
-          />
-        </div>
       </div>
     </div>
   );
